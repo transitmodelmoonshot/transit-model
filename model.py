821acb13adcf3325974b5b22ef0f3f30149d16e6
HyperbolicStudios <markinoe@gmail.com>
2022-02-16 14:51:51 -0800

    Updated code that actually works

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import pandas
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-df = pandas.read_csv("C:\Users\marki\Documents\GitHub\transit-model\data.csv")
+df = pandas.read_csv(r"C:\Users\marki\Documents\GitHub\transit-model\data.csv")
 # importing train_test_split from sklearn
 
-X = df[['Revenue Hours','Restaurant Bookings','Gas Price (C/L)','University School Season','Employment','WFH','Population Growth Rate','BC Vaccination Rate	hospitalizations']]
-y = df['Total boardings']
+x = df[['Revenue Hours','Restaurant Bookings','Gas Price (C/L)','University School Season','Employment','WFH','Population Growth Rate','BC Vaccination Rate',	'hospitalizations']]
+y = df['Total Boardings']
 
 
 # splitting the data
@@ -13,9 +13,9 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 
 regr = linear_model.LinearRegression()
 regr.fit(x_train, y_train)
-
+y_prediction = regr.predict(x_test)
 #predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
-y_prediction = regr.predict([[0,0,0,0,0,0,0,0,0]])
+#y_prediction = regr.predict([[0,0,0,0,0,0,0,0,0]])
 
 print(y_prediction)
 
@@ -23,7 +23,7 @@ print(y_prediction)
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 # predicting the accuracy score
-#score=r2_score(y_test,y_prediction)
-#print(‘r2 socre is ‘,score)
+score=r2_score(y_test,y_prediction)
+print(score)
 #print(‘mean_sqrd_error is==’,mean_squared_error(y_test,y_prediction))
 #print(‘root_mean_squared error of is==’,np.sqrt(mean_squared_error(y_test,y_prediction)))
