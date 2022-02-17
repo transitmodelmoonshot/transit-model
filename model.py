c5c949070c8f08db975a54cbede2b1c82fba59f1
HyperbolicStudios <markinoe@gmail.com>
2022-02-17 13:45:06 -0800

    Clean up

--- model.py
+++ model.py
@@ -4,14 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 import math
+import os
+from inspect import getsourcefile
+from os.path import abspath
 
-df = pandas.read_csv(r"C:\Users\marki\Documents\GitHub\transit-model\data.csv")
-# importing train_test_split from sklearn
+#set active directory to app location
+directory = abspath(getsourcefile(lambda:0))
+newDirectory = directory[:(directory.rfind("\\")+1)]
+os.chdir(newDirectory)
 
-#x = df[['Revenue Hours','Restaurant Bookings','Gas Price (C/L)','University School Season','Employment','WFH','Population Growth Rate','BC Vaccination Rate',	'hospitalizations']]
+df = pandas.read_csv(r"data.csv")
+
+#exclude the following columns from the explanatory dataset
 x = df.drop(['Total Boardings','ISOYear','Month','ISOWeek','Start of Week'],axis=1)
 y = df['Total Boardings']
-
 
 # splitting the data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
@@ -19,10 +25,9 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, rando
 regr = linear_model.LinearRegression()
 regr.fit(x_train, y_train)
 y_prediction = regr.predict(x_test)
-#predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
-#y_prediction = regr.predict([[0,0,0,0,0,0,0,0,0]])
 
-# importing r2_score module
+#Predict a specific value
+#y_prediction = regr.predict([[0,0,0,0,0,0,0,0,0]])
 
 # predicting the accuracy score
 Rsq=r2_score(y_test,y_prediction)
@@ -32,8 +37,6 @@ coeff_df = pandas.DataFrame(regr.coef_, x.columns, columns=['Coefficient'])
 print(coeff_df)
 N = 156
 K = 9
-fstat = (Rsq/(1-Rsq))*((N-K-1)/K) #you should find N and K yourself
 
 fstat = (Rsq/(1-Rsq))*((N-K-1)/K) #you should find N and K yourself
-#print(‘root_mean_squared error of is==’,np.sqrt(mean_squared_error(y_test,y_prediction)))
 print(fstat)
